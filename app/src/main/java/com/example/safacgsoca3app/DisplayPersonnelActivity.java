3ad29a07000117b02ecb95f2a4ada91a69c271d7
BamesJond117 <104476724+BamesJond117@users.noreply.github.com>
2022-05-23 13:54:52 +0800

    Changed from hardcoded placeholder data to draw data from db

--- app/src/main/java/com/example/safacgsoca3app/DisplayPersonnelActivity.java
+++ app/src/main/java/com/example/safacgsoca3app/DisplayPersonnelActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -19,8 +21,10 @@ import java.util.HashMap;
 
 public class DisplayPersonnelActivity extends AppCompatActivity {
 
-    public static final String TAG_ID_1 = "a_id";
-    public static final String TAG_ID_2 = "b_id";
+    public static final String TAG_ID = "p_id";
+    public static final String TAG_RANK = "p_rank";
+    public static final String TAG_NAME = "p_name";
+    public static final String TAG_REMARKS = "p_remarks";
     //Defines a key for the data. TAG_ID variable is for ease of changing the key
     //In hashmap, key is column name, index is row
     //Rest of app references key as TAG_ID. Key is currently "a_id"
@@ -30,6 +34,29 @@ public class DisplayPersonnelActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_personnel);
 
+        SQLiteDatabase db;
+        db = openOrCreateDatabase("A3App.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS Personnel (p_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,p_rank varchar(10) NOT NULL, p_name varchar(255) NOT NULL, p_remarks TEXT NOT NULL)");
+        Cursor c1 = db.rawQuery("select * from Personnel", null);
+
+        ArrayList<HashMap<String, String>> PersonnelList = new ArrayList<HashMap<String, String>>();
+        while (c1.moveToNext()) {
+            HashMap<String, String> map = new HashMap<String, String>();
+            String line_id = c1.getString(0);
+            String line_rank = c1.getString(1);
+            String line_name = c1.getString(2);
+            String line_remarks = c1.getString(3);
+
+            map.put(TAG_ID, line_id);
+            map.put(TAG_RANK, line_rank);
+            map.put(TAG_NAME, line_name);
+            map.put(TAG_REMARKS, line_remarks);
+
+            PersonnelList.add(map);
+        }
+        db.close();
+
+        /*
         ArrayList<HashMap<String, String>> PersonnelList = new ArrayList<HashMap<String, String>>();
         //Defines an array of hashmaps. Each hashmap contains multiple values associated to one key each
         //PersonnelList is the array of hashmaps
@@ -46,15 +73,15 @@ public class DisplayPersonnelActivity extends AppCompatActivity {
             //Puts key and data into hashmap
             PersonnelList.add(map);
             //Adds a hashmap into the array
-        }
+        }*/
 
         ListView lv = findViewById(R.id.lv_Issue_Detail_Info);
         ListAdapter adapter = new SimpleAdapter(
                 DisplayPersonnelActivity.this, //context
                 PersonnelList, //hashmapdata
                 R.layout.list_display_personnel, //layout of list
-                new String[]{TAG_ID_1,TAG_ID_2}, //from array
-                new int[]{R.id.Personnel_Name_Checklist, R.id.Personnel_Remarks_Checklist}); //toarray
+                new String[]{TAG_ID,TAG_RANK,TAG_NAME,TAG_REMARKS}, //from array
+                new int[]{R.id.Personnel_ID_Checklist, R.id.Personnel_Rank_Checklist, R.id.Personnel_Name_Checklist, R.id.Personnel_Remarks_Checklist}); //toarray
         // updating listview
         lv.setAdapter(adapter);
 
