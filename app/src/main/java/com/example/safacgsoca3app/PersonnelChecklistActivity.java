7f586bb8aa9c95ccefa25d65917606c0ff8a91e4
BamesJond117 <104476724+BamesJond117@users.noreply.github.com>
2022-06-19 15:26:03 +0800

    Checklist pass

--- app/src/main/java/com/example/safacgsoca3app/PersonnelChecklistActivity.java
+++ app/src/main/java/com/example/safacgsoca3app/PersonnelChecklistActivity.java
@@ -18,6 +18,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class PersonnelChecklistActivity extends AppCompatActivity {
 
@@ -47,6 +48,7 @@ public class PersonnelChecklistActivity extends AppCompatActivity {
         tv_D_ID.setText(d_id);
         tv_Personnel_Checklist_Detail_Name.setText(d_name);
 
+        ArrayList<HashMap<String, String>> PersonnelList = new ArrayList<HashMap<String, String>>();
         HashMap<String, String> PersonnelSelectionMap = new HashMap<String, String>();
 
         SQLiteDatabase db;
@@ -54,7 +56,7 @@ public class PersonnelChecklistActivity extends AppCompatActivity {
         db.execSQL("CREATE TABLE IF NOT EXISTS personnel (p_id integer NOT NULL PRIMARY KEY AUTOINCREMENT, p_rank varchar(255) NOT NULL, p_name varchar(255) NOT NULL, p_nric text)");
         Cursor c1 = db.rawQuery("select op.op_id, p.p_rank, p.p_name, p.p_nric from personnel p, operation_personnel op where op.p_id = p.p_id and op.o_id = " + o_id + " and op.d_id = " + d_id, null);
 
-        ArrayList<HashMap<String, String>> PersonnelList = new ArrayList<HashMap<String, String>>();
+
         while (c1.moveToNext()) {
             HashMap<String, String> map = new HashMap<String, String>();
             String line_id = c1.getString(0);
@@ -62,13 +64,13 @@ public class PersonnelChecklistActivity extends AppCompatActivity {
             String line_name = c1.getString(2);
             String line_remarks = c1.getString(3);
 
-            map.put("initialvalue","false");
+            map.put("initialvalue", "false");
             map.put(TAG_ID, line_id);
             map.put(TAG_RANK, line_rank);
             map.put(TAG_NAME, line_name);
             map.put(TAG_REMARKS, line_remarks);
+            map.put("SELECTED", "FALSE");
             PersonnelSelectionMap.put(line_id, "0");
-            Log.i(" ", String.valueOf(PersonnelSelectionMap));
 
             PersonnelList.add(map);
         }
@@ -79,12 +81,12 @@ public class PersonnelChecklistActivity extends AppCompatActivity {
                 PersonnelChecklistActivity.this, //context
                 PersonnelList, //hashmapdata
                 R.layout.list_personnel_checklist, //layout of list
-                new String[]{"initialvalue",TAG_ID,TAG_RANK,TAG_NAME,TAG_REMARKS}, //from array
+                new String[]{"initialvalue", TAG_ID, TAG_RANK, TAG_NAME, TAG_REMARKS}, //from array
                 new int[]{R.id.tv_Personnel_Selected_Status,
-                          R.id.tv_Personnel_ID_Checklist,
-                          R.id.tv_Personnel_Rank_Checklist,
-                          R.id.tv_Personnel_Name_Checklist,
-                          R.id.tv_Personnel_NRIC_Checklist});//toarray
+                        R.id.tv_Personnel_ID_Checklist,
+                        R.id.tv_Personnel_Rank_Checklist,
+                        R.id.tv_Personnel_Name_Checklist,
+                        R.id.tv_Personnel_NRIC_Checklist});//toarray
 
         // updating listview
         lv.setAdapter(adapter);
@@ -96,33 +98,60 @@ public class PersonnelChecklistActivity extends AppCompatActivity {
                 TextView select = (TextView) view.findViewById(R.id.tv_Personnel_Selected_Status);
                 TextView selectid = (TextView) view.findViewById(R.id.tv_Personnel_ID_Checklist);
 
-                if(select.getText().toString() == "false")
-                {
+                if (select.getText().toString() == "false") {
                     select.setText("true");
                     layout.setBackgroundResource(R.color.teal);
                     PersonnelSelectionMap.put(selectid.getText().toString(), "1");
-                    Log.i(" ", String.valueOf(PersonnelSelectionMap));                    ;
-                }else {
+                    Log.i(" ", String.valueOf(PersonnelSelectionMap));
+                    ;
+                } else {
                     select.setText("false");
                     layout.setBackgroundResource(R.color.white);
                     PersonnelSelectionMap.put(selectid.getText().toString(), "0");
                     Log.i(" ", String.valueOf(PersonnelSelectionMap));
                 }
-
             }
-
         });
 
         Button btnProceed = (Button) findViewById(R.id.btn_Proceed_Declare_Issue_Expend);
         btnProceed.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
+                for(Entry<String, String> entry: PersonnelSelectionMap.entrySet())
+                {
+                    if (entry.getValue() == "1")
+                    {
+                        for (int i = 0; i<PersonnelList.size(); i++)
+                        {
+                            if(PersonnelList.get(i).get(TAG_ID).equals(entry.getKey()))
+                            {
+                                PersonnelList.get(i).put("SELECTED", "TRUE");
+                                Log.i(" ", "Selected id: " + entry.getKey());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i<PersonnelList.size(); i++)
+                        {
+                            if(PersonnelList.get(i).get(TAG_ID).equals(entry.getKey()))
+                            {
+                                PersonnelList.get(i).put("SELECTED", "FALSE");
+                            }
+                        }
+                    }
+                }
+
+
+
+                Log.i(" ", String.valueOf(PersonnelList));
                 Intent intent = new Intent(PersonnelChecklistActivity.this, DeclareIssueReturnReceiveInfoActivity.class);
                 String Function3 = Function2;
                 intent.putExtra("Function3", Function3);
                 intent.putExtra("o_id", o_id);
                 intent.putExtra("d_id", d_id);
                 intent.putExtra("d_name", d_name);
+                intent.putExtra("PersonnelList", PersonnelList);
                 PersonnelChecklistActivity.this.startActivity(intent);
             }
         });
